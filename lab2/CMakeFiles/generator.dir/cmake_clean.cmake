b1f6d6fb3416259caca9d1c87d519177a98a836a
Nikita Rumiantsev <nilita.rumyan@gmail.com>
2022-12-25 14:34:55 +0300

    first commit

---
+++ lab2/CMakeFiles/generator.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/generator.dir/source.cpp.o"
+  "CMakeFiles/generator.dir/thread_num.cpp.o"
+  "generator"
+  "generator.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/generator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
